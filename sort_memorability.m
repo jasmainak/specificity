8a189464d9d7183f504fa7d1dd8a59d0365a5ec6
Mainak Jas <mainakjas@googlemail.com>
2014-03-16 21:13:31 -0400

    FIX: Refactor sort_memorability.m into three scripts according to functionality. One script sorts the memorability and needs to be run only once. The second script creates the images and the third creates the csv file

--- sort_memorability.m
+++ sort_memorability.m
@@ -3,19 +3,11 @@
 
 %% Specify directories
 clear all; close all;
-
-proj_dir = '/home/mainak/Desktop/specificity';
-data_dir = '/library/cvpr_memorability_data/Data/Experiment data';
-img_dir = '/library/cvpr_memorability_data/Data/Image data';
-
-addpath([proj_dir '/library/cvpr_memorability_code/Code/Library/cell2csv']);
-
-fprintf('Loading memorability images ...');
+data_dir = '../library/cvpr_memorability_data/Data/Experiment data';
 
 % load the data
-load([proj_dir data_dir '/sorted_target_data']);
-load([proj_dir img_dir '/SUN_urls.mat']);
-
+fprintf('Loading memorability images ...');
+load([data_dir '/sorted_target_data']);
 fprintf('[Done]');
 
 %% calculate memorability and sort it
@@ -32,55 +24,4 @@ end
 mem = hits./(hits+misses);
 [~, idx] = sort(mem, 'descend');
 
-%% sample images
-
-fprintf('\nSampling and writing images ...');
-
-% load images
-load([proj_dir img_dir '/target_images.mat']);
-
-n_samples = 222; % number of images to sample
-sample_gap = floor(N/n_samples); % gap between two samples
-
-% write n_samples images from the memorability data set
-mapping = zeros(1,n_samples);
-for i=1:n_samples
-    I = img(:,:,:,idx((i-1)*sample_gap + 1));
-    mapping(i) = idx((i-1)*sample_gap + 1);
-    imwrite(I, [proj_dir '/data/sampled_images/img_' num2str(i) '.jpg']);    
-end
-
-fprintf('[Done]');
-
-%% write csv file
-
-fprintf('\nWriting csv file for Amazon Mechanical Turk ...');
-
-for i=1:n_samples/3
-    mturk_csv{i+1,1} = ['https://neuro.hut.fi/~mainak/sampled_images/img_' num2str(i*3-2) '.jpg'];
-    mturk_csv{i+1,2} = ['https://neuro.hut.fi/~mainak/sampled_images/img_' num2str(i*3-1) '.jpg'];
-    mturk_csv{i+1,3} = ['https://neuro.hut.fi/~mainak/sampled_images/img_' num2str(i*3) '.jpg'];
-end
-
-mturk_csv{1,1} = 'image1_url'; mturk_csv{1,2} = 'image2_url';
-mturk_csv{1,3} = 'image3_url';
-
-cell2csv([proj_dir '/data/mturk/input/mturk_descriptions1.csv'],mturk_csv);
-
-fprintf('[Done]');
-
-%% Produce html file
-
-fprintf('\nWriting html file ...');
-
-fp = fopen([proj_dir '/data/sampled_images/index.html'],'w');
-for i=1:n_samples
-    fwrite(fp,['<img src="img_' num2str(i) '.jpg"><br/>']);
-    fwrite(fp,sprintf('Score=%0.2f<br/>',mem(idx((i-1)*sample_gap + 1))));
-end
-fclose(fp);
-
-save([proj_dir '/data/memorability_mapping.mat'],'mapping', 'mem');
-clearvars -except mapping img;
-
-fprintf('[Done]\n');
+save('../data/memorability_sorted.mat','mem', 'idx');
