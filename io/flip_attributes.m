ec4017845202de252eb7415d68119ed4dd196dfd
Mainak Jas <mainakjas@googlemail.com>
2014-10-20 16:03:01 +0300

    Move some scripts to io/ folder

--- io/flip_attributes.m
+++ io/flip_attributes.m
@@ -1,14 +1,14 @@
 close all; clear all;
 
-load('../library/annotations/annotations/anno_feats.mat');
-load('../library/annotations/annotations/anno_names.mat');
+load('../../library/annotations/annotations/anno_feats.mat');
+load('../../library/annotations/annotations/anno_names.mat');
 
 attr = {'enclosed_space', 'perspective_view', 'empty_space', ...
         'mirror_symmetry', 'pleasant_scene', 'unusual_scene', 'dull_colors', ...
         'expert_photography', 'attractive', 'memorable', 'central_object',...
         'single_focus', 'zoomed_in', 'top_down', 'sky_present', 'clear_sky', ...
         'blue_sky'};
-    
+
 for i=1:length(attr)
     idx(i) = find(strcmpi(anno_names, attr{i}));
 end
@@ -22,4 +22,4 @@ sky_idx = find(anno_feats(:, strcmpi(anno_names, 'sky_present')) < 0.5);
 anno_feats(sky_idx, strcmpi(anno_names, 'clear_sky')) = 0;
 anno_feats(sky_idx, strcmpi(anno_names, 'blue_sky')) = 0;
 
-save('../library/annotations/annotations/anno_feats_modified.mat', 'anno_feats');
+save('../../library/annotations/annotations/anno_feats_modified.mat', 'anno_feats');
