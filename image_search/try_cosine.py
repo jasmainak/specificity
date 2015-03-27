e545e79aa5b754932ec62b94ba793895cad43b3a
Mainak Jas <mainakjas@googlemail.com>
2015-03-27 15:28:57 +0100

    remove redundant file

--- image_search/try_cosine.py
+++
@@ -1,7 +0,0 @@
-from utils.similarity import find_sentence_similarity
-
-sent1 = 'Two young men trying to fix their car together'
-sent2 = 'The lot at the Toyota dealership is crowded with cars.'
-
-sim = find_sentence_similarity([sent2], [sent1], dataset_name='memorability',
-                               method='cosine')
